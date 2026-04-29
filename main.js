2fa7c65398928ccf833b6c2fd4af52bb9e5b7736
Khainesus <akke2@seznam.cz>
2026-04-29 10:41:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import { generateEmployeeData, getEmployeeStatistics } from "./src/employees.js";
 
+/**
+ * Main function
+ * @param {object} dtoIn - input data
+ * @returns {object} dtoOut - output data
+ */
 export function main(dtoIn) {
   const employees = generateEmployeeData(dtoIn);
   const result = getEmployeeStatistics(employees);
