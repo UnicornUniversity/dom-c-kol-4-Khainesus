153cab4b06bb9973f9b9fbbe2e7a6df59911ea96
Khainesus <akke2@seznam.cz>
2026-04-29 10:37:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
-import { generateGender, generateName, generateSurname, generateWorkload, generateBirthdate, validateInput } from "./src/employees.js";
-
-export function generateEmployeeData(dtoIn) {
-  validateInput(dtoIn);
-
-  const employees = [];
-
-  for (let i = 0; i < dtoIn.count; i++) {
-    const gender = generateGender();
-    const name = generateName(gender);
-    const surname = generateSurname(gender);
-    const workload = generateWorkload();
-    const birthdate = generateBirthdate(dtoIn.age.min, dtoIn.age.max);
-
-    employees.push({
-      gender,
-      name,
-      surname,
-      birthdate,
-      workload,
-    });
-  }
-
-  return employees;
-}
-
-export function getEmployeeStatistics(employees) {
-  
-}
+import { generateEmployeeData, getEmployeeStatistics } from "./src/employees.js";
 
 export function main(dtoIn) {
   const employees = generateEmployeeData(dtoIn);
-  const dtoOut = getEmployeeStatistics(employees);
-  return dtoOut;
+  const result = getEmployeeStatistics(employees);
+  return result;
 }
